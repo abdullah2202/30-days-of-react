ab722736fcd13480e4f128246723cf52f251d3a9
Mohammad Abdullah <abdullah_22@hotmail.com>
2022-07-28 12:32:02 +0100

    Day 3

--- src/index.js
+++ src/index.js
@@ -1,18 +1,94 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import avatarImage from './images/avatar.jpg'
 
-const jsxElement = <h1>This is a JSX Element</h1>;
-const rootElement = document.getElementById('root');
-
-const title = <h2>Getting Started React</h2>;
+const welcome = 'Welcome to 30 Days of React'
+const title = 'Getting Started with React'
+const subtitle = 'Javascript Library'
+const author = {
+  firstName: 'Mohammed',
+  lastName: 'Abdullah',
+}
+const date = 'Jul 28, 2022'
+const user = (
+  <div>
+    <img src={avatarImage} alt='Avatar Image' width='200px' />
+  </div>
+)
 
 const header = (
   <header>
-    <h1>Welcome to 30 Days React</h1>
-    <h2>Getting Started React</h2>
-    <h3>Javascript Library</h3>
+    <div className='header-wrapper'>
+      <h1>{welcome}</h1>
+      <h2>{title}</h2>
+      <h3>{subtitle}</h3> 
+      <p>
+        Instructor: {author.firstName} {author.lastName}
+      </p>
+      <small>Date: {date}</small>
+    </div>
   </header>
-);
+)
 
-ReactDOM.render(header, rootElement);
+const numOne = 3
+const numTwo = 2
+
+const result = (
+  <p>
+    {numOne} + {numTwo} = {numOne + numTwo}
+  </p>
+)
+
+const yearBorn = 1820
+const currentYear = new Date().getFullYear()
+const age = currentYear - yearBorn
+const personAge = (
+  <p>
+    {' '}
+    {author.firstName} {author.lastName} is {age} years old
+  </p>
+)
+
+const techs = ['HTML', 'CSS', 'JavaScript']
+const techsFormatted = techs.map((tech) => <li>{tech}</li>)
+
+const main = (
+  <main>
+    <div className='main-wrapper'>
+      <p>Prerequisite to get started{' '}
+        <strong>
+          <em>react.js</em>
+        </strong>
+        :
+      </p>
+      <ul>
+        {techsFormatted}
+      </ul>
+      {result}
+      {personAge}
+      {user}
+    </div>
+  </main>
+)
+
+const copyRight = 'Copyright 2022'
+
+const footer = (
+  <footer>
+    <div className='footer-wrapper'>
+      <p>{copyRight}</p>  
+    </div>
+  </footer>
+)
+
+const app = (
+  <div>
+    {header}
+    {main}
+    {footer}
+  </div>
+)
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(app, rootElement)
 
